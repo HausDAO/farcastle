9f3aafc83a551733fdb4a40f330cf1366baf9e93
earth2travis <travis.mccutcheon@gmail.com>
2024-10-21 11:26:25 -0500

    Update navigation

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,108 +1,107 @@
-import { defineConfig } from "vocs";
+import { defineConfig } from 'vocs';
 
 export default defineConfig({
-  title: "Farcastle",
-  logoUrl: "/images/alice-blue-castle.svg",
-  topNav: [{ text: "About", link: "/about" }],
+  title: 'Farcastle',
+  logoUrl: '/images/alice-blue-castle.svg',
   socials: [
     {
-      icon: "warpcast",
-      link: "https://warpcast.com/~/channel/farcastle",
+      icon: 'warpcast',
+      link: 'https://warpcast.com/~/channel/farcastle',
     },
     {
-      icon: "github",
-      link: "https://github.com/hausDao",
+      icon: 'github',
+      link: 'https://github.com/HausDAO/farcastle-frames',
     },
   ],
-  description: "Turn a Warpcast channel into a DAO",
+  description: 'Turn a Warpcast channel into a DAO',
   ogImageUrl: {
-    "/": "/images/preview.png",
+    '/': '/images/preview.png',
   },
   theme: {
-    accentColor: "#00B1CC",
+    accentColor: '#00B1CC',
     variables: {
       color: {
         background: {
-          light: "#EAF8FF",
-          dark: "#341A34",
+          light: '#EAF8FF',
+          dark: '#341A34',
         },
       },
     },
   },
   font: {
     default: {
-      google: "VT323",
+      google: 'VT323',
     },
   },
   sidebar: [
     {
-      text: "About",
-      link: "/about",
+      text: 'About',
+      link: '/about',
     },
     {
-      text: "Map",
-      link: "/map",
+      text: 'Map',
+      link: '/map',
     },
     {
-      text: "Vision",
-      link: "/vision",
+      text: 'Vision',
+      link: '/vision',
     },
     {
-      text: "Values",
-      link: "/values",
+      text: 'Values',
+      link: '/values',
     },
     {
-      text: "DAO",
+      text: 'DAO',
       collapsed: true,
       items: [
         {
-          text: "Roles",
-          link: "/dao/roles",
+          text: 'Roles',
+          link: '/dao/roles',
         },
       ],
     },
   ],
   sponsors: [
     {
-      name: "Grants",
+      name: 'Grants',
       height: 120,
       items: [
         [
           {
-            name: "DAOhaus",
-            link: "https://daohaus.club/",
-            image: "/images/daohaus.svg",
+            name: 'DAOhaus',
+            link: 'https://daohaus.club/',
+            image: '/images/daohaus.svg',
           },
         ],
       ],
     },
     {
-      name: "Allies",
+      name: 'Allies',
       height: 60,
       items: [
         [
           {
-            name: "Hats",
-            link: "https://www.hatsprotocol.xyz/",
-            image: "/images/hats.svg",
+            name: 'Hats',
+            link: 'https://www.hatsprotocol.xyz/',
+            image: '/images/hats.svg',
           },
           {
-            name: "Tavern",
-            link: "https://tavern.fm/",
-            image: "/images/tavern.svg",
+            name: 'Tavern',
+            link: 'https://tavern.fm/',
+            image: '/images/tavern.svg',
           },
         ],
       ],
     },
     {
-      name: "Accelerators",
+      name: 'Accelerators',
       height: 120,
       items: [
         [
           {
-            name: "Octant",
-            link: "https://octant.app/",
-            image: "/images/octant.svg",
+            name: 'Octant',
+            link: 'https://octant.app/',
+            image: '/images/octant.svg',
           },
         ],
       ],
